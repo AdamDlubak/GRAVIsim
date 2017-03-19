78e101838ed654dbe318f3437040f588bd2156e4
Jakub Błaszczyk <j.blaszczyk95@gmail.com>
2017-03-19 21:24:47 +0100

    Resize canvas. Add guidelines

--- static/directives/simulator.directive.js
+++ static/directives/simulator.directive.js
@@ -10,6 +10,7 @@
                         var canvas = element.find('canvas').get()[0];
                         var ctx = canvas.getContext('2d');
                         var url = '/static/media/test-simulator.json';
+                        var size = 1600;
 
                         scope.fps = 5;
                         scope.pause = true;
@@ -33,13 +34,26 @@
                         }
 
                         function draw(frame) {
-                            ctx.clearRect(0, 0, 640, 480);
+                            ctx.clearRect(0, 0, size, size);
+                            ctx.strokeStyle = 'rgb(0, 200, 0)';
+                            ctx.lineWidth = 1;
+
+                            ctx.beginPath();
+                            ctx.moveTo(0, size / 2);
+                            ctx.lineTo(size, size / 2);
+                            ctx.stroke();
+
+                            ctx.beginPath();
+                            ctx.moveTo(size / 2, 0);
+                            ctx.lineTo(size / 2, size);
+                            ctx.stroke();
+
                             ctx.fillStyle = 'rgb(255, 255, 255)';
 
                             var timeline = scope.data.timeline[frame];
                             timeline.forEach(function(position, index) {
                                 ctx.beginPath();
-                                ctx.arc(position[0] + 320, position[1] + 240,
+                                ctx.arc(position[0] + size / 2, position[1] + size / 2,
                                         scope.data.particles[index].mass / 2,
                                         0, Math.PI * 2, true);
                                 ctx.fill();
