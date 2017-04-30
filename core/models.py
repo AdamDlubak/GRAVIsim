de172ac0367660117a20cb1890e1af23692d4caf
Jakub Błaszczyk <j.blaszczyk95@gmail.com>
2017-04-30 23:50:41 +0200

    Implement Spark Job Scheduler

--- core/models.py
+++ core/models.py
@@ -1,21 +1,32 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# # Create your models here.
+SparkJobStates = {
+    'WAITING': '0',
+    'RUNNING': '1',
+    'FINISHED': '2',
+}
 
+class SparkJob(models.Model):
+    STATE_TYPES = tuple([(i, x) for x, i in SparkJobStates.items()])
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     phone = models.CharField(max_length=20, blank=True, default='')
-#     website = models.TextField(max_length=125, blank=True)
+    name = models.TextField(max_length=100)
+    description = models.TextField(max_length=255, blank=True, null=True)
+    author = models.ForeignKey(User)
+    state = models.CharField(max_length=1, choices=STATE_TYPES)
+    priority = models.SmallIntegerField(default=5)
+    created = models.DateTimeField(default=datetime.now)
+    started = models.DateTimeField(blank=True, null=True)
+    finished = models.DateTimeField(blank=True, null=True)
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+    def get_state_name(self, index):
+        return list(SparkJobStates.keys())[list(SparkJobStates.values()).index(index)]
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+    def __str__(self):
+        return "SparkJob #{} (Priority: {}) : {} : {}".format(
+            self.id,
+            self.priority,
+            self.get_state_name(self.state),
+            self.name
+        )
