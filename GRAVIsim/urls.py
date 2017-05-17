f1ce14511b08a095d885e2369aea71333a64f485
Adam Dłubak <adam.dlubak@gmail.com>
2017-05-17 10:41:12 +0200

    Start working at login page and URLs

--- GRAVIsim/urls.py
+++ GRAVIsim/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
   #  url(r'^logout/$', 'django.contrib.auth.views.logout'),
 
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^login', TemplateView.as_view(template_name='login.html'), name='index'),
+    
     url(r'^/?.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
