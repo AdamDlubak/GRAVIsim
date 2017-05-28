e8bdb1e9fd8b55ea8f2b61e39e0215dad3f4811c
Adam Dłubak <adam.dlubak@gmail.com>
2017-05-29 01:35:40 +0200

    Refactoring - My Account, Contact, About -- Clear styles, fixed grid

--- static/controllers/my-account.controller.js
+++ static/controllers/my-account.controller.js
@@ -1,6 +1,6 @@
 (function(){
     angular.module('gravisim').
-        controller('MyaccountController', ['$scope', '$http', '$location', 'authentication',
+        controller('MyAccountController', ['$scope', '$http', '$location', 'authentication',
             function($scope, $http, $location, $authentication) {
                 var self = this;
 
